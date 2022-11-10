0ab3ed8c4024c45ea3aaf8c3866940d788e57ab7
yasminMVieira <minmarques2001@gmail.com>
2022-11-10 10:56:29 -0300

    first commit

---
+++ pages/index.js
@@ -0,0 +1,116 @@
+import config from "../config.json"
+import styled from "styled-components";
+import { CSSReset } from "../src/components/CSSReset";
+import Menu from "../src/components/Menu";
+import { StyledTimeline } from "../src/components/Timeline";
+import React from "react";
+
+function HomePage() {
+
+    const [valorDoFiltro, setValorDoFiltro] = React.useState("");
+
+    //console.log(config.playlists);
+
+    return (
+        <>
+            <CSSReset />
+            <div style={{
+                display: "flex",
+                flexDirection: "column",
+                flex: 1,
+                backgroundColor: "white",
+            }}>
+                <Menu valorDoFiltro={valorDoFiltro} setValorDoFiltro={setValorDoFiltro} />
+                <Header />
+                <Timeline searchValue={valorDoFiltro} playlists={config.playlists} />
+            </div>
+        </>
+    );
+}
+
+export default HomePage
+
+/* function Menu() {
+    return (
+        <div>
+            Menu
+        </div>
+    )
+} */
+
+const StyledHeader = styled.div` 
+    img{
+        width: 80px;
+        height: 80px;
+        border-radius: 50%
+    }
+    .user-info {
+        //margin-top: 50px;
+        display: flex;
+        align-content: center;
+        width: 100%;
+        padding: 16px 32px;
+        gap: 16px;
+    }
+`;
+const StyledBanner = styled.div` 
+    background-color: blue;
+    background-image: url('https://images.unsplash.com/photo-1524008279394-3aed4643b30b?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1074&q=80');
+    height: 250px;
+`;
+function Header() {
+    return (
+        <StyledHeader>
+            <StyledBanner />
+            <section className="user-info">
+                <img src={`https://github.com/${config.github}.png`} />
+                <div>
+                    <h2>
+                        {config.nome}
+                    </h2>
+                    <p>
+                        {config.job}
+                    </p>
+                </div>
+            </section>
+        </StyledHeader>
+    )
+}
+
+function Timeline({ searchValue, ...props }) {
+    //console.log("Dentro do comp", props.playlists);
+    const playlistNames = Object.keys(props.playlists);
+
+    return (
+        <StyledTimeline>
+            {playlistNames.map(function (playlistNames) { //pode usar playlistNames.map((playlistNames) => {...}
+                const videos = props.playlists[playlistNames]
+
+                return (
+                    <section>
+                        <h2>
+                            {playlistNames}
+                        </h2>
+                        <div>
+                            {videos.filter((video) => {
+                                const titleNormalized = video.title.toLowerCase();
+                                const searchValueNormalized = searchValue.toLowerCase();
+                                return titleNormalized.includes(searchValueNormalized)
+                            }).map((video) => {
+                                return (
+                                    <a href={video.url}>
+                                        <img src={video.thumb} />
+                                        <span>
+                                            {video.title}
+                                        </span>
+                                    </a>
+                                )
+                            })}
+                        </div>
+
+                    </section>
+                )
+            })}
+        </StyledTimeline>
+    )
+}
